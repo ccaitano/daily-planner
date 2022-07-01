7e561d9fd6849ce375a14b1c0fb2286d8a97ced0
ccaitano <cheryl.caitano@gmail.com>
2022-06-30 17:33:26 -1000

    Correct timeblocks now displayed to the page

--- js/script.js
+++ js/script.js
@@ -10,12 +10,20 @@ currentTimeEl.textContent = currentTime;
 
 //Display Time Blocks with Save Button
 var timeBlocksEl = document.querySelector(".container");
-// var displayTimeBlocksEl = document.createElement('div');
-var hour = moment().format('h a');
-var hourTemplate = `
+var amPM = "AM";
+for (var i = 9; i <= 17; i++) {
+    //Need to get previously saved data for each hour
+    var hour = i;
+    if (i === 12) {
+        amPM = "PM";
+    } else if (i > 11) {
+        amPM = "PM";
+        hour = i-12;
+    }
+    var hourTemplate = `
     <div class="row">
         <div>
-            ${hour}
+            ${hour}${amPM}
         </div>
         <div>
             <textarea></textarea>
@@ -24,11 +32,5 @@ var hourTemplate = `
             <button data-hour="${i}">Save</button>
         </div>
     </div>`;
-
-for (var i = 9; i <= 18; i++) {
-    //Need to get previously saved data for each hour
-    hour = i;
-    console.log(hour);
     timeBlocksEl.innerHTML += hourTemplate;
-
 }
