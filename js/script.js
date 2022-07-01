424d2bf66898572aeb6c83c0e195cc839b88868a
ccaitano <cheryl.caitano@gmail.com>
2022-06-30 17:43:14 -1000

    Added next steps to comments

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ var timeBlocksEl = document.querySelector(".container");
 var amPM = "AM";
 for (var i = 9; i <= 17; i++) {
     //Need to get previously saved data for each hour
+    //Need to compare current time to hour to determine if hour is in the past, present or future
     var hour = i;
     if (i === 12) {
         amPM = "PM";
@@ -22,15 +23,22 @@ for (var i = 9; i <= 17; i++) {
     }
     var hourTemplate = `
     <div class="row">
-        <div>
-            ${hour}${amPM}
+        <div class="hour">
+            <p>${hour}${amPM}</p>
+        </div>
+        <div class="time-block">
+            <textarea class="description"></textarea>
         </div>
         <div>
-            <textarea></textarea>
-        </div>
-        <div>
-            <button data-hour="${i}">Save</button>
+            <button data-hour="${i}" class="saveBtn">Save</button>
         </div>
     </div>`;
     timeBlocksEl.innerHTML += hourTemplate;
 }
+//Need to set up a click event listener on the container
+
+//Need to get the hour from the clicked button's (event.target) "data-hour" attribute
+
+//Use the hour to create a key for local storage
+
+
