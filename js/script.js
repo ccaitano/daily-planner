e4049f537bdf2bd9710c7c2b24fcd15f0347050d
ccaitano <cheryl.caitano@gmail.com>
2022-06-30 17:52:23 -1000

    Added code to check if current hour is 'past', 'present', or 'future'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Declare Current Day and Time
 var todayDate = moment().format('dddd, MMMM Do, YYYY');
 var currentTime = moment().format('h:mm:ss a');
+var currentHour = moment().format('HH');
 
 //Create Day and Time Elements
 var todayDateEl = document.getElementById("currentDay");
@@ -11,9 +12,18 @@ currentTimeEl.textContent = currentTime;
 //Display Time Blocks with Save Button
 var timeBlocksEl = document.querySelector(".container");
 var amPM = "AM";
+console.log(currentHour);
 for (var i = 9; i <= 17; i++) {
     //Need to get previously saved data for each hour
-    //Need to compare current time to hour to determine if hour is in the past, present or future
+    //Compare current time to hour to determine if hour is in the past, present or future
+    if (i > currentHour) {
+        console.log('future');
+    } else if (i === currentHour) {
+        console.log('present');
+    } else {
+        console.log('past');
+    }
+
     var hour = i;
     if (i === 12) {
         amPM = "PM";
