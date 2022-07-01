ea29f468682f1551403119185d05c2cbb42a16cb
ccaitano <cheryl.caitano@gmail.com>
2022-06-30 17:20:46 -1000

    Starting code to add time blocks to the page

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ var currentTimeEl = document.getElementById("currentTime");
 currentTimeEl.textContent = currentTime;
 
 //Display Time Blocks with Save Button
+var timeBlocksEl = document.querySelector(".container");
+// var displayTimeBlocksEl = document.createElement('div');
+var hour = moment().format('h a');
+var hourTemplate = `
+    <div class="row">
+        <div>
+            ${hour}
+        </div>
+        <div>
+            <textarea></textarea>
+        </div>
+        <div>
+            <button data-hour="${i}">Save</button>
+        </div>
+    </div>`;
+
+for (var i = 9; i <= 18; i++) {
+    //Need to get previously saved data for each hour
+    hour = i;
+    console.log(hour);
+    timeBlocksEl.innerHTML += hourTemplate;
+
+}
