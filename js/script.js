56dab0219c1b8e4edc296d93b4e2586c815b871b
ccaitano <cheryl.caitano@gmail.com>
2022-07-03 13:53:56 -1000

    Set onclick listener to associate buttons with corresponding text input fields

--- js/script.js
+++ js/script.js
@@ -13,11 +13,11 @@ currentTimeEl.textContent = currentTime;
 var timeBlocksEl = document.querySelector(".container");
 var amPM = "AM";
 
-console.log(currentHour);
 for (var i = 9; i <= 17; i++) {
     //Need to get previously saved data for each hour
+    // var key = "hour-"+i;
+
     //Compare current time to hour to determine if hour is in the past, present or future
-    console.log(i);
     if (i > currentHour) {
         timeStamp = "future";
     } else if (i == currentHour) {
@@ -25,7 +25,7 @@ for (var i = 9; i <= 17; i++) {
     } else if (i < currentHour) {
         timeStamp = "past";
     }
-
+    //Set AM/PM values
     var hour = i;
     if (i === 12) {
         amPM = "PM";
@@ -33,22 +33,35 @@ for (var i = 9; i <= 17; i++) {
         amPM = "PM";
         hour = i-12;
     }
+    //Set Template for each timeblock
     var hourTemplate = `
     <div id="hour-${i}" class="row no-gutters ${timeStamp}">
         <div class="hour col">
             <p>${hour}${amPM}</p>
         </div>
         <div class="time-block col-6">
-            <textarea class="description ${timeStamp}"></textarea>
+            <textarea id="input-${i}" class="description ${timeStamp}"></textarea>
         </div>
-        <div class="col">
-            <button data-hour="${i}" class="saveBtn">SAVE 💾</button>
+        <div class="btncol col">
+            <button id="${i}" class="saveBtn">SAVE 💾</button>
         </div>
     </div>`;
+    //Create timeblock for each hour
     timeBlocksEl.innerHTML += hourTemplate;
+    //Create Button Elements
+    
 }
-//Need to set up a click event listener on the container
 
+//Need to set up a click event listener on the container
+var containerEl = document.querySelector(".container");
+
+containerEl.addEventListener("click", function(event) {
+    console.log(event.target.id);
+    var key = event.target.id;
+    var textEl = document.getElementById("input-"+key);
+    console.log(textEl.value);
+    // localStorage.setItem("hour-"+key, JSON.stringify(savedData));
+});
 //Need to get the hour from the clicked button's (event.target) "data-hour" attribute
 
 //Use the hour to create a key for local storage
